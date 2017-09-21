be8f6c4d9b1fb6d4ea554e0f98ec1cc6c9c14481
nklapste-ubuntu-dev <nklapste@ualberta.ca>
2017-09-21 14:49:49 -0600

    adding main files

---
+++ src/cookieCutter.java
@@ -0,0 +1,16 @@
+/**
+ * Assignment 2: Java Regular Expressions
+ */
+
+public class cookieCutter {
+
+    /**
+     * Main entry
+     *
+     * @param args {@code String[]} Command line arguments
+     */
+    public static void main(String[] args) {
+
+    } // public static void main(String[] args)
+
+}
